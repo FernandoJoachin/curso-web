ad5103d3df0c100dbd33ac8f2c0c6de8884654cb
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-04-27 00:39:37 -0600

    Seccion24 avance

---
+++ seccion24/03-tipos-dato.php
@@ -0,0 +1,21 @@
+<?php include 'includes/header.php';
+//Booleano
+$logueado = false;
+var_dump($logueado);
+
+//Entero
+$numero = 150;
+var_dump($numero);
+
+//Float
+$flotante = 240.54;
+var_dump($flotante);
+
+//String
+$nombre = "Fernando";
+var_dump($nombre);
+
+//Array
+$arreglo = [];
+var_dump($arreglo);
+include 'includes/footer.php';
---
+++ seccion24/04-operadores.php
@@ -0,0 +1,27 @@
+<?php include 'includes/header.php';
+$numero1 = 20;
+$numero2 = 10;
+$numero3 = 5.6;
+
+//Sumar
+echo $numero1 + $numero2;
+echo "<br>";
+echo $numero1 + $numero3;
+echo "<br>";
+
+//Resta
+echo $numero1 - $numero2;
+echo "<br>";
+
+//Multiplicar
+echo $numero1 * $numero2;
+echo "<br>";
+
+//Dividir
+echo $numero2 / $numero1;
+echo "<br>";
+
+//Potencia
+echo 2 ** 3;
+echo "<br>";
+include 'includes/footer.php';
